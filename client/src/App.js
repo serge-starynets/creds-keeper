280dbf10f93deaed5f56c89657543428fc7e316d
sergeincode <starynets.sergey@gmail.com>
2019-08-06 17:27:47 +0300

    add auth state

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,27 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
-import CredState  from './context/cred/CredState';
+import CredState from './context/cred/CredState';
+import AuthState from './context/auth/AuthState';
 
 const App = () => {
   return (
-    <CredState>
-    <Router>
-    <Fragment>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-        </Switch>
-      </div>
-    </Fragment>
-    </Router>
-    </CredState>
+    <AuthState>
+      <CredState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </CredState>
+    </AuthState>
   );
-}
+};
 
 export default App;
