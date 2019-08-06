9c82e0a4d096f01c2ca70f9286da47cea0424d64
sergeincode <starynets.sergey@gmail.com>
2019-08-06 18:23:01 +0300

    add login and register pages

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,6 +12,8 @@ const Navbar = ({ title, icon1, icon2 }) => {
         <li>
           <Link to='/'>Home</Link>
           <Link to='/about'>About</Link> 
+          <Link to='/login'>Login</Link> 
+          <Link to='/register'>Register</Link> 
         </li>
       </ul>
       
