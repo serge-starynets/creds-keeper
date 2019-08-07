f43b755e280ad50b9c714a78d313acfe4f8183e5
sergeincode <starynets.sergey@gmail.com>
2019-08-07 23:17:45 +0300

    add login logout

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import PrivateRoute from './components/routing/PrivateRout';
 
 import CredState from './context/cred/CredState';
 import AuthState from './context/auth/AuthState';
@@ -28,7 +29,7 @@ const App = () => {
             <div className="container">
               <Alerts />
               <Switch>
-                <Route exact path="/" component={Home} />
+                <PrivateRoute exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -65,13 +65,32 @@ const AuthState = props => {
   };
 
   // Login User
-  const loginUser = () => {
-    console.log('');
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
   };
 
   // Logout
   const logout = () => {
-    console.log('');
+    dispatch({ type: LOGOUT });
   };
 
   // Clear Errors
@@ -89,7 +108,7 @@ const AuthState = props => {
         error: state.error,
         register,
         loadUser,
-        loginUser,
+        login,
         logout,
         clearErrors
       }}
